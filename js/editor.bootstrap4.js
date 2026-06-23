66a637895086017b604607e76a2ed21e77d06c0a
Allan Jardine <allan@datatables.net>
2026-06-23 15:23:48 +0000

    Initial commit

---
+++ js/editor.bootstrap4.js
@@ -0,0 +1,273 @@
+/*! Editor Bootstrap 4 styling 3.0.0-dev for DataTables
+ * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
+ */
+
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-bs4', 'datatables.net-editor'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-bs4')(root);
+			}
+
+			if (! window.DataTable.Editor) {
+				require('datatables.net-editor')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+
+//
+// Note that this file does use jQuery as Bootstrap 4's JS depends on jQuery for
+// its modal, so we know that it must be present.
+//
+
+// Set the default display controller to be our bootstrap control 
+DataTable.Editor.defaults.display = "bootstrap";
+
+// Change the default classes from Editor to be classes for Bootstrap
+DataTable.util.object.assignDeep(DataTable.Editor.classes, {
+	"header": {
+		"wrapper": "DTE_Header modal-header",
+		title: {
+			tag: 'h5',
+			class: 'modal-title'
+		}
+	},
+	"body": {
+		"wrapper": "DTE_Body modal-body"
+	},
+	"footer": {
+		"wrapper": "DTE_Footer modal-footer"
+	},
+	"form": {
+		"tag": "form-horizontal",
+		"button": "btn",
+		"buttonInternal": "btn btn-outline-secondary",
+		"buttonSubmit": 'btn btn-primary'
+	},
+	"field": {
+		"wrapper": "DTE_Field form-group row",
+		"label":   "col-lg-4 col-form-label",
+		"input":   "col-lg-8 DTE_Field_Input",
+		"inputError": 'is-invalid',
+		"error":   "error is-invalid",
+		"msg-labelInfo": "form-text text-secondary small",
+		"msg-info":      "form-text text-secondary small",
+		"msg-message":   "form-text text-secondary small",
+		"msg-error":     "form-text text-danger small",
+		"multiValue":    "card multi-value",
+		"multiInfo":     "small",
+		"multiRestore":  "multi-restore"
+	}
+} );
+
+DataTable.util.object.assignDeep(DataTable.ext.buttons, {
+	create: {
+		formButtons: {
+			className: 'btn-primary'
+		}
+	},
+	edit: {
+		formButtons: {
+			className: 'btn-primary'
+		}
+	},
+	remove: {
+		formButtons: {
+			className: 'btn-danger'
+		}
+	}
+} );
+
+DataTable.Editor.fieldTypes.datatable.tableClass = 'table';
+
+/*
+ * Bootstrap display controller - this is effectively a proxy to the Bootstrap
+ * modal control.
+ */
+
+let shown = false;
+let fullyShown = false;
+
+const dom = {
+	content: null,
+	close: null
+};
+
+DataTable.Editor.display.bootstrap = DataTable.util.object.assignDeep({}, DataTable.Editor.models.displayController, {
+	init: function ( dte ) {
+		var $ = DataTable.use('jq');
+
+		if (!dom.content) {
+			// Note that `modal-dialog-scrollable` is BS4.3+ only. It has no effect on 4.0-4.2
+			dom.content = $(
+				'<div class="modal fade DTED">'+
+					'<div class="modal-dialog modal-lg modal-dialog-scrollable"></div>'+
+				'</div>'
+			);
+
+			dom.close = $('<button class="close">&times;</div>');
+		}
+
+		// Add `form-control` to required elements
+		dte.on( 'displayOrder.dtebs open.dtebs', function () {
+			$.each( dte.s.fields, function ( key, field ) {
+				$('input:not([type=checkbox]):not([type=radio]), select, textarea', field.node() )
+					.addClass( 'form-control' );
+			} );
+		} );
+
+		return DataTable.Editor.display.bootstrap;
+	},
+
+	open: function ( dte, append, callback ) {
+		var $ = DataTable.use('jq');
+
+		$(append).addClass('modal-content');
+
+		// Special class for DataTable buttons in the form
+		$(append).find('div.DTE_Field_Type_datatable div.dt-buttons')
+			.removeClass('btn-group')
+			.addClass('btn-group-vertical');
+
+		// Setup events on each show
+		dom.close
+			.attr('title', dte.i18n.close)
+			.off('click.dte-bs4')
+			.on('click.dte-bs4', function () {
+				dte.close('icon');
+			})
+			.appendTo($('div.modal-header', append));
+
+		// This is a bit horrible, but if you mousedown and then drag out of the modal container, we don't
+		// want to trigger a background action.
+		let allowBackgroundClick = false;
+		$(document)
+			.off('mousedown.dte-bs4')
+			.on('mousedown.dte-bs4', 'div.modal', function (e) {
+				allowBackgroundClick = $(e.target).hasClass('modal') && shown
+					? true
+					: false;
+			} );
+
+		$(document)
+			.off('click.dte-bs4')
+			.on('click.dte-bs4', 'div.modal', function (e) {
+				if ( $(e.target).hasClass('modal') && allowBackgroundClick ) {
+					dte.background();
+				}
+			} );
+
+		var content = dom.content.find('div.modal-dialog');
+		content.children().detach();
+		content.append( append );
+
+		if ( shown ) {
+			if ( callback ) {
+				callback();
+			}
+			return;
+		}
+
+		shown = true;
+		fullyShown = false;
+
+		$(dom.content)
+			.one('shown.bs.modal', function () {
+				// Can only give elements focus when shown
+				if ( dte.s.setFocus ) {
+					dte.s.setFocus.focus();
+				}
+
+				fullyShown = true;
+
+				dom.content.find('table.dataTable').DataTable().columns.adjust();
+
+				if ( callback ) {
+					callback();
+				}
+			})
+			.one('hidden', function () {
+				shown = false;
+			})
+			.appendTo( 'body' )
+			.modal( {
+				backdrop: "static",
+				keyboard: false
+			} );
+	},
+
+	close: function ( dte, callback ) {
+		var $ = DataTable.use('jq');
+
+		if ( ! shown ) {
+			if ( callback ) {
+				callback();
+			}
+			return;
+		}
+
+		// Check if actually displayed or not before hiding. BS4 doesn't like `hide`
+		// before it has been fully displayed
+		if ( ! fullyShown ) {
+			$(dom.content)
+				.one('shown.bs.modal', function () {
+					DataTable.Editor.display.bootstrap.close( dte, callback );
+				} );
+
+			return;
+		}
+
+		$(dom.content)
+			.one( 'hidden.bs.modal', function () {
+				$(this).detach();
+			} )
+			.modal('hide');
+
+		shown = false;
+		fullyShown = false;
+
+		if ( callback ) {
+			callback();
+		}
+	},
+
+	node: function () {
+		return dom.content[0];
+	}
+} );
+
+
+return DataTable.Editor;
+}));
---
+++ types/types.d.ts
@@ -0,0 +1,7 @@
+
+// Dist-DataTables-Editor-Bootstrap4 integration with Bootstrap4 exports the main Editor class having
+// set default values to complete the ingeration.
+import Editor from "datatables.net-editor";
+
+export default Editor;
+
